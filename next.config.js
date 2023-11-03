bc1361e3f912145ae1f935f6c624fa154dc04618
lavabyte <buweiliao@gmail.com>
2023-11-03 11:29:45 +0100

    Add support for subdomains

--- next.config.js
+++ next.config.js
@@ -3,8 +3,26 @@ const withMDX = require("@next/mdx")()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// Configure `pageExtensions`` to include MDX files
-	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"]
-	// Optionally, add any other Next.js config below
+	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+	// Add support for subdomains
+	async rewrites() {
+		return {
+			beforeFiles: [
+				// if the host is `loglock.extropy.dev`,
+				// this rewrite will be applied
+				{
+					source: "/:path*",
+					has: [
+						{
+							type: "host",
+							value: "loglock.extropy.dev"
+						}
+					],
+					destination: "/loglock/:path*"
+				}
+			]
+		}
+	}
 }
 
 module.exports = withMDX(nextConfig)
