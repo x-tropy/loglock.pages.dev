acceec49f1255d6b9fb832f06cce0bd10f33060e
lavabyte <buweiliao@gmail.com>
2023-11-09 11:35:12 +0100

    Serve pdf files through worker.

--- app/loglock/presentation/page.js
+++ app/loglock/presentation/page.js
@@ -3,7 +3,7 @@ import Iframe from "react-iframe"
 export default function Page() {
 	return (
 		<div className='w-[960px] mx-auto'>
-			<Iframe url='https://extropy.dev/files/BuweiLiao_Presentation_KIT.pdf' width='100%' height='600px' display='block' position='relative' />
+			<Iframe url='https://static.buweiliao.workers.dev/BuweiLiao_Presentation_KIT.pdf' width='100%' height='600px' display='block' position='relative' />
 		</div>
 	)
 }
